27a721b91428dd5c003f62ed017b5bfe92d7af4e
christophe Dubois <dubois51ch@gmail.com>
2023-06-26 11:46:31 +0200

    Uploads

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
 
 const router = express.Router();
+const upload = multer({ dest: "./public/uploads/" });
+const { v4: uuidv4 } = require("uuid");
 
 const itemControllers = require("./controllers/itemControllers");
+
+// route POST pour recevoir un fichier
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+
+      res.send("File uploaded");
+    }
+  );
+});
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
